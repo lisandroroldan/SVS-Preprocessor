9a81aa71201a15bd6689d63d0f46159ab9395deb
lisandroroldan <lisandroroldan@users.noreply.github.com>
2018-10-23 12:19:44 -0300

    line segment data
    -partial GUI programation

--- segment_structure.m
+++ segment_structure.m
@@ -3,7 +3,7 @@ function  segment_structure
 global SVS
 
 n=size(SVS.INPUT_GEOMETRY.line_sections,1);
-
+c=0; %counter
 for i=1:n
     id_n=SVS.INPUT_GEOMETRY.line_sections(i,1);
     starting_node_n=SVS.INPUT_GEOMETRY.line_sections(i,2);
